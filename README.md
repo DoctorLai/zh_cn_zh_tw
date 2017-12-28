02b4db252ab3830b5bdfe99e4e59ae9ce58e217b
SteakOverCooked <Oh@SteakOverCooked.com>
2017-12-28 19:37:04 +0000

    init release

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # zh_cn_zh_tw
 NodeJs Library to convert between Simplified Chinese (zh-cn) and Traditional Chinese (zh-tw)
+
+
+## Installation
+```
+npm install zh_cn_zh_tw --save
+```
+
+## Usage
+```
+var convertor = require('zh_cn_zh_tw');
+var zh_cn = converter.convertToSimplifiedChinese;
+var zh_tw = converter.convertToTraditionalChinese;
+console.log(zh_cn("夢")); // convert to 梦
+console.log(zh_tw("梦")); // convert to 夢
+```
+
+## Tests
+```
+npm test
+```
+
+## Contributing
+[@justyy](https://steemit.com/@justyy)
+
+## Release Notes
+* 1.0.0 Initial Release
---
+++ test/test.js
@@ -0,0 +1,23 @@
+var should = require('chai').should(),
+    convertor = require('../index'),
+    zh_cn = convertor.convertToSimplifiedChinese,
+    zh_tw = convertor.convertToTraditionalChinese;
+
+describe('zh_cn', function() {
+  it('converts 夢', function() {
+    zh_cn('夢').should.equal('梦');
+  });
+  it('converts 梦', function() {
+    zh_cn('梦').should.equal('梦');
+  });  
+});
+
+describe('zh_tw', function() {
+  it('converts 梦', function() {
+    zh_cn('梦').should.equal('夢');
+  });
+  it('converts 夢', function() {
+    zh_cn('夢').should.equal('夢');
+  });  
+});
+
